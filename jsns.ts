f7961d023fef641f0bb1e01121488b15018bdfe6
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-30 09:12:41 -0400

    Switched jsns to typescript, already found an error. Nice.

--- jsns.ts
+++ jsns.ts
@@ -1,37 +1,46 @@
-var jsns = (function () {
+﻿var jsns = (function () {
     "use strict";
+
     var loaded = {};
     var unloaded = {};
     var runners = [];
+
     function isModuleLoaded(name) {
         return loaded[name] !== undefined;
     }
+
     function isModuleLoadable(name) {
         return unloaded[name] !== undefined;
     }
-    function loadModule(name) {
+
+    function loadModule(name){
         var loaded = checkLib(unloaded[name]);
         if (loaded) {
             delete unloaded[name];
         }
         return loaded;
     }
+
     function setModuleLoaded(name, module) {
         if (loaded[name] === undefined) {
             loaded[name] = module;
         }
     }
+
     function Module(name) {
         var loadingDelayed = false;
         var self = this;
+
         this.exports = {};
+
         /**
          * Figure out if this module is delay loading.
          * @returns {bool} True if delay loading, false if fully loaded
          */
-        this.isLoadingDelayed = function () {
+        this.isLoadingDelayed = function(){
             return loadingDelayed;
-        };
+        }
+
         /**
          * Set this module to delay loading mode, you must call setLoaded manually
          * after calling this function or the module will never be considered loaded.
@@ -39,7 +48,8 @@ var jsns = (function () {
          */
         this.delayLoading = function () {
             loadingDelayed = true;
-        };
+        }
+
         /**
          * Set the module to loaded. Only needs to be called if delayLoading is called,
          * otherwise there is no need.
@@ -47,12 +57,14 @@ var jsns = (function () {
         this.loaded = function () {
             setModuleLoaded(name, self);
             loadRunners();
-        };
+        }
     }
+
     function checkLib(library) {
         var dependencies = library.dependencies;
         var fullyLoaded = true;
         var module = undefined;
+
         //Check to see if depenedencies are loaded and if they aren't and can be, load them
         for (var i = 0; i < dependencies.length; ++i) {
             var dep = dependencies[i];
@@ -62,26 +74,33 @@ var jsns = (function () {
             }
             fullyLoaded = fullyLoaded && dep.loaded;
         }
+
         //If all dependencies are loaded, load this library
         if (fullyLoaded) {
             module = new Module(library.name);
             var args = [module.exports, module];
+
             //Inject dependency arguments
             for (var i = 0; i < dependencies.length; ++i) {
                 var dep = dependencies[i];
                 args.push(loaded[dep.name].exports);
             }
+
             library.factory.apply(module, args);
+
             if (!module.isLoadingDelayed()) {
                 setModuleLoaded(library.name, module);
             }
         }
+
         return fullyLoaded && !module.isLoadingDelayed();
     }
+
     function Library(name, depNames, factory) {
         this.name = name;
         this.factory = factory;
         this.dependencies = [];
+
         if (depNames) {
             for (var i = 0; i < depNames.length; ++i) {
                 var depName = depNames[i];
@@ -92,6 +111,7 @@ var jsns = (function () {
             }
         }
     }
+
     function loadRunners() {
         for (var i = 0; i < runners.length; ++i) {
             var runner = runners[i];
@@ -100,11 +120,13 @@ var jsns = (function () {
             }
         }
     }
+
     function recursiveWaitingDebug(name, indent) {
         var indentStr = '';
         for (var i = 0; i < indent; ++i) {
             indentStr += ' ';
         }
+
         var module = unloaded[name];
         if (module !== undefined) {
             console.log(indentStr + module.name);
@@ -119,15 +141,18 @@ var jsns = (function () {
             console.log(indent + name + ' module not yet loaded.');
         }
     }
+
     return {
         run: function (dependencies, factory) {
             runners.push(new Library("AnonRunner", dependencies, factory));
             loadRunners();
         },
+
         define: function (name, dependencies, factory) {
             unloaded[name] = new Library(name, dependencies, factory);
             loadRunners();
         },
+
         debug: function () {
             if (runners.length > 0) {
                 for (var i = 0; i < runners.length; ++i) {
@@ -145,6 +170,5 @@ var jsns = (function () {
                 console.log("No runners remaining.");
             }
         }
-    };
+    }
 })();
-//# sourceMappingURL=jsns.js.map
