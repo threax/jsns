3e62948d910c7fa23f946b24143f86d6c685f498
Andrew Piper <andrew@threax.com>
2021-07-21 17:57:58 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/jsns.git
+git push https://github.com/threax/jsns.git --tags
+git push https://gitlab.com/threax-software/jsns.git
+git push https://gitlab.com/threax-software/jsns.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/jsns
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/jsns --tags
