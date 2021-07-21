f17139ca5eb2eaa1b5c69d855c26b9dceff6475a
Andrew Piper <andrew@threax.com>
2021-07-21 18:26:08 -0400

    Change runattributes into an artifact.

--- runattributes.js
+++ runattributes.js
@@ -1,9 +1,4 @@
 "use strict";
-
-declare var jsns;
-
-//Find all data-hr-run attributes and run the runner they specify, it does not matter what kind of element
-//contains the runner.
 var runnerElements = document.querySelectorAll('[data-hr-run]');
 for (var i = 0; i < runnerElements.length; ++i) {
     var runnerElement = runnerElements[i];
@@ -12,5 +7,3 @@ for (var i = 0; i < runnerElements.length; ++i) {
         jsns.run(runnerAttr);
     }
 }
-
-export var ran = true; //Dummy operation to force this to be a module
